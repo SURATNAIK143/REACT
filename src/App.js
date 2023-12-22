ec6f1414403b5811aa449b67a5a83b47fce1b41b
SURATNAIK143 <suratnaik7@gmail.com>
2023-12-22 09:28:28 +0530

    d1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import './App.css';
-import Video from './components/Video';
-function App() {
-  return (
-    <div className="App">
-      <Video title='Education Channel' views='12k' like='1m'/>
-      <Video title='Learning Channel' views='1102k' like='11m'/>
-      <Video title='Reading Channel' views='104k' like='18m'/>
+import "./App.css";
 
-    </div>
-  );
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
